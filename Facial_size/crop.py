989770b579e05f1cfb9dca8df710473652ec3f9a
Sirin-890 <bappamorya639@gmail.com>
2025-05-05 03:20:31 +0530

    eye size added

---
+++ Facial_size/crop.py
@@ -0,0 +1,37 @@
+import os
+from huggingface_hub import hf_hub_download
+from ultralytics import YOLO
+from supervision import Detections
+from PIL import Image
+import numpy as np
+from loguru import logger
+import cv2
+
+def face_detection_crop(image_path,output_path):
+    #image= cv2.imread(image_path)
+    """
+    Detects a face in a single image, crops it, and returns the cropped face image.
+    """
+    model_path = hf_hub_download(repo_id="arnabdhar/YOLOv8-Face-Detection", filename="model.pt")
+    model = YOLO(model_path)
+    
+    image = Image.open(image_path)
+    output = model(image)
+    results = Detections.from_ultralytics(output[0])
+    bounding_boxes = output[0].boxes.xywh.cpu().numpy()
+    
+    if len(bounding_boxes) == 0:
+        logger.warning("No face detected in the image.")
+        return None
+    
+    x, y, w, h = bounding_boxes[0]  
+    left = int(x - w / 2)
+    right = int(x + w / 2)
+    top = int(y - h / 2)
+    bottom = int(y + h / 2)
+    
+    face_crop = image.crop((left, top, right, bottom))
+    face_crop.save(output_path)
+    
+    logger.info("Face detection and cropping completed.")
+    return x,y,w,h
---
+++ Facial_size/size_calculations.py
@@ -0,0 +1,47 @@
+import cv2
+from crop import face_detection_crop
+import math
+import mediapipe as mp
+def euclidean_distance(x1,y1,x2,y2):
+    return   math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def eye_size(img_path):
+    li=[]
+    out_path=""
+    _,_,w,h=face_detection_crop(img_path,img_path)
+    img_temp=cv2.imread(img_path)
+    img= cv2.cvtColor(img_temp,cv2.COLOR_BGR2RGB)
+    face_mesh_mp=mp.solutions.face_mesh
+    face_mesh=face_mesh_mp.FaceMesh(static_image_mode=True)
+    face_point=face_mesh.process(img)
+    if face_point.multi_face_landmarks:
+        for landmark_list in face_point.multi_face_landmarks:
+            l=[130,243,159,145,463,359,386,374]
+            
+            for i in l:
+                li.append((landmark_list.landmark[i].x * w,landmark_list.landmark[i].y * h))
+
+    
+    right_side_width=euclidean_distance(li[0][0],li[0][1],li[1][0],li[1][1])
+    right_side_height=euclidean_distance(li[2][0],li[2][1],li[3][0],li[3][1])
+    left_side_width=euclidean_distance(li[4][0],li[4][1],li[5][0],li[5][1])
+    left_side_height=euclidean_distance(li[6][0],li[6][1],li[7][0],li[7][1])
+    
+    for i in li:
+        cv2.circle(img, (int(i[0]), int(i[1])), 5, (0, 255, 0), -1)
+        cv2.imshow("Face Landmarks", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return right_side_width,right_side_height,left_side_width,left_side_height
+
+   
+if __name__ == "__main__":
+    p1=""
+    p2=""
+    print(eye_size(p1))
+    print(eye_size(p2))
+
+
+
+
+
